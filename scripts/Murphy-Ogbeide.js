8435bc4a723c73b990fb03acb6cc97cd54407460
Murphy Ogbeide <61960212+Murphite@users.noreply.github.com>
2020-06-04 09:27:43 +0000

    Update Murphy-Ogbeide.js

--- scripts/Murphy-Ogbeide.js
+++ scripts/Murphy-Ogbeide.js
@@ -1,5 +1,3 @@
-//console.log("Hello World, this is Murphy Ogbeide with HNGi7 ID-01770 and email ogbeidemurphy@gmail.com using JavaScript for stage 2 task.");
-
 const fullname = "Murphy Ogbeide";
 const hngid = "HNG-01770";
 const language = "Javascript";
